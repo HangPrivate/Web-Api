050242b42d225107b1745dcfa0bafe42cbf23e88
Bampukis <danzpunyaaa@gmail.com>
2025-05-27 21:05:47 +0700

    Add files via upload

---
+++ src/api/tools/text2qr.js
@@ -0,0 +1,15 @@
+module.exports = function app (app) {
+app.get('/imagecreator/brat', async (req, res) => {
+        try {
+            const { text } = req.query
+            const pedo = await getBuffer(`https://api.siputzx.my.id/api/tools/text2qr?text=${text}`)
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': pedo.length,
+            });
+            res.end(pedo);
+        } catch (error) {
+            res.status(500).send(`Error: ${error.message}`);
+        }
+    });
+}
